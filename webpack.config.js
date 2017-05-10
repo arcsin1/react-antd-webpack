120e4cc27a9f7773d34eb6c099352a688b585d40
zijian <zijian@konge.com>
2017-05-10 16:53:26 +0800

    refactor:优化webpack打包和md5

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,44 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");  //css单独打包
 var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
 
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var WebpackMd5Hash = require('webpack-md5-hash');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var copy = require('quickly-copy-file');
+var del = require('del');
+// 开发环境
+var isDev = function() {
+  return process.env.NODE_ENV.trim() === 'development';
+};
+
+// 生产环境
+var isProd = function() {
+  return process.env.NODE_ENV.trim() === 'production';
+};
+
+copyAndDelFiles();
+
 module.exports = {
-    devtool: 'eval-source-map',
+    devtool: isProd() ? false : 'inline-source-map',
 
     entry: {
         main: './src/entry.js', //唯一入口文件
-        vendor: ['react']
+        vendor: [
+          'react',
+          'react-dom',
+          'react-router',
+          'react-redux',
+          'redux',
+          'redux-thunk',
+        ]
     },
     output: {
-        path: './build', //打包后的文件存放的地方
-        filename: 'main.js', //打包后输出文件的文件名
-        publicPath: 'http://localhost:8080/build/'  //启动本地服务后的根目录
+        path: './build',
+        filename: isProd() ? '[name].[chunkhash:8].js' : '[name].js',
+        chunkFilename: isProd() ? '[name].chunk.[chunkhash:8].js' : '[name].chunk.js',
+        publicPath: isProd() ? './build/' : '/build/'
     },
 
     module: {
@@ -38,20 +65,86 @@ module.exports = {
         require('autoprefixer')    //调用autoprefixer插件,css3自动补全
     ],
 
-    devServer: {
-        // contentBase: './src/views'  //本地服务器所加载的页面所在的目录
-        port: 8080,
-        colors: true,  //终端中输出结果为彩色
-        historyApiFallback: true,  //不跳转
-        inline: true  //实时刷新
-    },
+    // devServer: {
+    //     port: 8888,
+    //     colors: true,  //终端中输出结果为彩色
+    //     historyApiFallback: true,  //不跳转
+    //     inline: true  //实时刷新
+    // },
 
-    plugins: [
-        new ExtractTextPlugin('main.css'),
-        new CommonsChunkPlugin({
-           name: 'vendor',
-           filename: 'vendor.js'
-        })
-    ]
+    // plugins: [
+    //     new ExtractTextPlugin('main.css'),
+    //     new CommonsChunkPlugin({
+    //        name: 'vendor',
+    //        filename: 'vendor.js'
+    //     })
+    // ]
+     plugins: getPlugins()
 
+}
+
+// 复制和删除文件
+function copyAndDelFiles() {
+  var copyFile = '';
+
+  // 复制文件
+  if (isDev()) {
+    copyFile = 'src/views/index_dev.html';
+  }
+
+  if (isProd()) {
+    copyFile = 'src/views/index.html';
+  }
+
+  copy(copyFile, 'index.html', function(error) {
+    if (error) {
+      return console.error(error);
+    }
+  });
+
+  if (isProd()) {
+    del(['dist']);
+  }
+}
+
+
+// 获取配置
+function getPlugins() {
+  var plugins = [
+    new webpack.DefinePlugin({
+      __DEV__ : isDev(),
+      __PROD__: isProd(),
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV.trim())
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendor', isProd() ? 'vendor.[chunkhash:8].js' : 'vendor.js'),
+    new ExtractTextPlugin(isProd() ? '[name].[chunkhash:8].css' : '[name].css'),
+  ];
+
+  if (isDev()) {
+    plugins.push(
+      new OpenBrowserPlugin({ url: 'http://localhost:8080/' })
+    );
+  }
+
+  if (isProd()) {
+    plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        output: {
+          comments: false,
+        },
+        compress: {
+          warnings: false
+        }
+      }),
+      new HtmlWebpackPlugin({
+        title: '空格',
+        filename: '../index.html',
+        template: './src/views/index.html'
+      }),
+      new WebpackMd5Hash()
+    );
+  }
+
+  return plugins
 }
